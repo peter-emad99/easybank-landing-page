16ada42fb00457ea30773178b89526b52e4a2f7a
Peter Emad <peteremad95@gmail.com>
2022-03-17 21:34:23 +0200

    Revert "fixing some issues"
    This reverts commit 7595f29ca355d244de2191a407bbb51aae575785.

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const body = document.querySelector("body");
-const html = document.querySelector("html");
-const menuIcon = document.querySelector(".hamburger-menu-icon");
-const menu = document.querySelector(".header-nav");
-const mobileHeaderImages = document.querySelector(".mobile-header-images");
-const IconToOpen = document.querySelector(".icon_to_Open");
-const IconToClose = document.querySelector(".icon_to_Close");
 
-menuIcon.addEventListener("click", function () {
-  console.log("click hamburger");
-  menu.classList.toggle("open_menu");
-  mobileHeaderImages.classList.toggle("overlay-shown");
-  IconToOpen.classList.toggle("hidden_icon");
-  IconToClose.classList.toggle("hidden_icon");
-  body.classList.toggle("noscroll");
-  html.classList.toggle("noscroll");
-});
+const body = document.querySelector('body');
+const menuIcon = document.querySelector('.hamburger-menu-icon');
+const menu = document.querySelector('.header-nav');
+const mobileHeaderImages = document.querySelector('.mobile-header-images');
+const IconToOpen = document.querySelector('.icon_to_Open')
+const IconToClose = document.querySelector('.icon_to_Close')
+
+
+
+menuIcon.addEventListener('click', function(){
+  console.log('click hamburger');
+menu.classList.toggle('open_menu');
+mobileHeaderImages.classList.toggle('overlay-shown')
+IconToOpen.classList.toggle('hidden_icon')
+IconToClose.classList.toggle('hidden_icon')
+body.classList.toggle('noscroll')
+
+
+}); 
